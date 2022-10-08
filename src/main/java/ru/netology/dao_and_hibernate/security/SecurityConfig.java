fecea23a4df79c98e7bdc705dfcec43d3f2c0f08
Sergei Prekrasnov <sap685@gmail.com>
2022-10-08 21:11:16 +0300

    security-OAuth2 ver.1

--- src/main/java/ru/netology/dao_and_hibernate/security/SecurityConfig.java
+++ src/main/java/ru/netology/dao_and_hibernate/security/SecurityConfig.java
@@ -1,14 +1,15 @@
 package ru.netology.dao_and_hibernate.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder encoder() {
@@ -20,22 +21,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user1")
                 .password("{noop}user1")
-                .authorities("read", "write")
+                .roles("READ", "WRITE", "DELETE")
                 .and()
                 .withUser("user2")
                 .password("{noop}user2")
-                .authorities("read");
+                .roles("READ")
+                .and()
+                .withUser("user3").password("{noop}password3")
+                .roles("WRITE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .and()
-                .authorizeRequests().antMatchers("/hello").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/save").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers("/count").hasAuthority("read")
+                .authorizeRequests().antMatchers("/hi").permitAll()
                 .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
